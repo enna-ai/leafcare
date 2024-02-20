8434f752bcbea0021eab3fa641782b0aefc1c641
enna-ai <annelee3322@gmail.com>
2024-02-19 19:48:47 -0700

    🚀🚀🚀

---
+++ server/firefly/src/test/java/com/enna_ai/firefly/FireflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enna_ai.firefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
