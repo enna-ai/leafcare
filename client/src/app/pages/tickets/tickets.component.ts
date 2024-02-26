4867019e3d8360f53a8fb1c1e7fa5e076fd95697
enna-ai <annelee3322@gmail.com>
2024-02-25 17:35:42 -0700

    ✨ add paginator base

--- client/src/app/pages/tickets/tickets.component.ts
+++ client/src/app/pages/tickets/tickets.component.ts
@@ -1,26 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ViewChild } from '@angular/core';
 import { TicketService } from '../../services/ticket.service';
 import { Ticket } from '../../interfaces/ticket.interface';
 import { Router } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { SidebarService } from '../../services/sidebar.service';
-
+import { MatPaginatorModule, MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, MatPaginatorModule],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.scss'
 })
 export class TicketsComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   sidebarService = inject(SidebarService);
   ticketService = inject(TicketService);
   tickets: Ticket[] = [];
   selectedTicket: string[] = [];
   filteredTickets: Ticket[] = [];
-  router = new Router();
+  pageSize: number = 10;
+
+  constructor(private router: Router) {
+    this.paginator = {} as MatPaginator;
+  }
 
   ngOnInit(): void {
     this.ticketService.getTickets().subscribe(tickets => {
@@ -60,5 +66,14 @@ export class TicketsComponent implements OnInit {
     } else {
       this.filteredTickets = [...this.tickets];
     }
+
+    if (this.paginator) {
+      this.paginator.firstPage();
+    }
+  }
+
+  onPageChange(event: PageEvent) {
+    const startIndex = event.pageIndex * event.pageSize;
+    this.filteredTickets = this.tickets.slice(startIndex, startIndex + event.pageSize);
   }
 }
